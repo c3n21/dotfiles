d556e86eea8b712420e3d4d0f4c0b7883620b1c5
Zhifan <chenzhifan9@gmail.com>
2021-05-17 18:09:59 +0200

    Added default font size && no startup screen

--- .config/emacs/init.el
+++ .config/emacs/init.el
@@ -25,6 +25,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(inhibit-startup-screen t)
  '(package-selected-packages '(gruvbox-theme)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
@@ -34,3 +35,5 @@
  )
 
 (load-theme 'gruvbox t)
+
+(set-face-attribute 'default nil :height 200)
