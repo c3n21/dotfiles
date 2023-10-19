7305a471595d5ab38eb2bea0cc95c05de18c5d40
c3n21 <chenzhifan9@gmail.com>
2023-10-19 18:17:48 +0200

    feat(nixos): cjk font

--- .config/nixos/configuration.nix
+++ .config/nixos/configuration.nix
@@ -206,8 +206,33 @@ let catppuccin-macchiato = pkgs.callPackage /home/zhifan/.config/nixos/catpuccin
     # If you want to use JACK applications, uncomment this
     #jack.enable = true;
   };
-  fonts.fontDir.enable = true;
-  fonts.fonts = [
-    delugia-code
-  ];
+  fonts = {
+    fontDir.enable = true;
+    fonts = with pkgs; [
+      noto-fonts
+      source-han-sans
+      source-han-serif
+      delugia-code
+    ];
+    fontconfig = {
+      defaultFonts = {
+        emoji = [ "Noto Color Emoji" ];
+        monospace = [
+          "Noto Sans Mono CJK SC"
+          "Sarasa Mono SC"
+          "DejaVu Sans Mono"
+        ];
+        sansSerif = [
+          "Noto Sans CJK SC"
+          "Source Han Sans SC"
+          "DejaVu Sans"
+        ];
+        serif = [
+          "Noto Serif CJK SC"
+          "Source Han Serif SC"
+          "DejaVu Serif"
+        ];
+      };
+    };
+  };
 }
