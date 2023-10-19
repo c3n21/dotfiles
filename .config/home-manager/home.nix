c9b89034c1dfb638cd7bf53348c03e7bff97746a
c3n21 <chenzhifan9@gmail.com>
2023-10-19 18:20:16 +0200

    fix(home-manager): fix error

--- .config/home-manager/home.nix
+++ .config/home-manager/home.nix
@@ -18,8 +18,8 @@
   # The home.packages option allows you to install Nix packages into your
   # environment.
   home.packages = with unstable-pkgs; [
+    neovim
     nixd
-    unstable-pkgs
     nixpkgs-fmt
     tmux
     libsForQt5.okular
