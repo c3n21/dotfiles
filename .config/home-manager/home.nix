dfb89fd07632990d3491a6a85f6504b564a4d628
c3n21 <chenzhifan9@gmail.com>
2023-10-20 10:11:27 +0200

    fix(home-manager): fcitx session variables

--- .config/home-manager/home.nix
+++ .config/home-manager/home.nix
@@ -94,7 +94,10 @@
   #
   # if you don't want to manage your shell through Home Manager.
   home.sessionVariables = {
-    # EDITOR = "emacs";
+    XMODIFIERS = "@im=fcitx";
+    XMODIFIER = "@im=fcitx";
+    GTK_IM_MODULE = "fcitx";
+    QT_IM_MODULE = "fcitx";
   };
 
   # Let Home Manager install and manage itself.
