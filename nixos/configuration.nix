de51df6debb92c68febdac8fdec2e2b2696b9a2a
c3n21 <chenzhifan9@gmail.com>
2024-05-01 16:16:15 +0200

    feat(nixos): "fs.inotify.max_queued_events" = 100000;

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -91,6 +91,7 @@ in {
     kernelPackages = pkgs.linuxKernel.packages.linux_zen;
     kernel.sysctl = {
       "vm.swappiness" = 10;
+      "fs.inotify.max_queued_events" = 100000;
     };
     loader = {
       efi.canTouchEfiVariables = true;
