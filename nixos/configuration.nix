5b860473cf4105ac7d9d2674cb4399727fb58e6d
c3n21 <chenzhifan9@gmail.com>
2024-05-18 11:18:14 +0200

    feat(nixos): use podman

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -166,7 +166,21 @@ in {
     };
   };
 
-  virtualisation.docker.enable = true;
+  # Enable common container config files in /etc/containers
+  virtualisation = {
+    containers = {
+      enable = true;
+    };
+    podman = {
+      enable = true;
+
+      # Create a `docker` alias for podman, to use it as a drop-in replacement
+      dockerCompat = true;
+
+      # Required for containers under podman-compose to be able to talk to each other.
+      defaultNetwork.settings.dns_enabled = true;
+    };
+  };
 
   # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users.zhifan = {
@@ -215,6 +229,11 @@ in {
     libsForQt5.qt5.qtgraphicaleffects
     javaPackages.openjfx17
     sbctl
+
+    # Podman
+    dive # look into docker image layers
+    podman-tui # status of containers in the terminal
+    podman-compose # start group of containers for dev
   ];
 
   programs = {
