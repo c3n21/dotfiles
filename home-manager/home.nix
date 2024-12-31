188d8e9a39c615e21f85516781a92e28b4f8c8cc
c3n21 <chenzhifan9@gmail.com>
2024-12-31 17:50:14 +0100

    don't enable vi mode inside neovim

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -213,14 +213,20 @@ in {
       enable = true;
       interactiveShellInit = ''
         set -x theme_color_scheme nord
-        fish_vi_key_bindings
+        if not set -q NVIM
+          fish_vi_key_bindings
+
           # Emulates vim's cursor shape behavior
           # Set the normal and visual mode cursors to a block
-        set fish_cursor_default block
+          set fish_cursor_default block
+
           # Set the insert mode cursor to a line
-        set fish_cursor_insert line
+          set fish_cursor_insert line
+
           # Set the replace mode cursor to an underscore
-        set fish_cursor_replace_one underscore
+          set fish_cursor_replace_one underscore
+        end
+
         bind -M insert \cf accept-autosuggestion
       '';
       shellAliases = {
