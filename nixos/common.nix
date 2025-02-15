3a0a21a40bcb5fe38d3787eb339cc48be1408247
c3n21 <chenzhifan9@gmail.com>
2025-02-15 15:15:02 +0100

    fix(nixos): firewall exceptions for virbr0

--- nixos/common.nix
+++ nixos/common.nix
@@ -1,7 +1,29 @@
 { ... }:
 {
   networking = {
-    firewall.enable = true;
+    firewall = {
+      trustedInterfaces = [ "virbr0" ];
+      enable = true;
+      # https://github.com/NixOS/nixpkgs/issues/263359 workaround for not getting DHCP and DNS
+      interfaces = {
+        # lxdbr0 = {
+        #   allowedTCPPorts = [ 53 ];
+        #   allowedUDPPorts = [
+        #     53
+        #     67
+        #   ];
+        # };
+        virbr0 = {
+          allowedTCPPorts = [ 53 ];
+          allowedUDPPorts = [
+            53
+            67
+          ];
+        };
+
+      };
+
+    };
     nftables.enable = true;
   };
 }
