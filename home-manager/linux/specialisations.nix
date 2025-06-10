d1797b230ef99c2a13495840c4a196baa327fd0e
c3n21 <37077738+c3n21@users.noreply.github.com>
2025-06-10 14:56:29 +0200

    feat(niri): brightnessctl

--- home-manager/linux/specialisations.nix
+++ home-manager/linux/specialisations.nix
@@ -238,12 +238,12 @@
 
             // Set open-maximized to true for all windows.
             window-rule {
-                open-maximized true
+              open-maximized true
             }
 
             // this seems to fix cursor issues with niri
             cursor {
-                xcursor-size ${builtins.toString config.home.pointerCursor.size}
+              xcursor-size ${builtins.toString config.home.pointerCursor.size}
             }
 
             environment {
@@ -260,75 +260,75 @@
 
             input {
               keyboard {
-                  xkb {
-                      layout "us"
-                      variant "altgr-intl"
-                  }
+                xkb {
+                  layout "us"
+                  variant "altgr-intl"
+                }
 
-                  // repeat-delay 600
-                  // repeat-rate 25
-                  // track-layout "global"
+                // repeat-delay 600
+                // repeat-rate 25
+                // track-layout "global"
               }
 
               touchpad {
-                  // off
-                  tap
-                  // dwt
-                  // dwtp
-                  natural-scroll
-                  // accel-speed 0.2
-                  // accel-profile "flat"
-                  // scroll-factor 1.0
-                  // scroll-method "two-finger"
-                  // scroll-button 273
-                  // tap-button-map "left-middle-right"
-                  // click-method "clickfinger"
-                  // left-handed
-                  // disabled-on-external-mouse
-                  // middle-emulation
+                // off
+                tap
+                // dwt
+                // dwtp
+                natural-scroll
+                // accel-speed 0.2
+                // accel-profile "flat"
+                // scroll-factor 1.0
+                // scroll-method "two-finger"
+                // scroll-button 273
+                // tap-button-map "left-middle-right"
+                // click-method "clickfinger"
+                // left-handed
+                // disabled-on-external-mouse
+                // middle-emulation
               }
 
               mouse {
-                  // off
-                  // natural-scroll
-                  // accel-speed 0.2
-                  // accel-profile "flat"
-                  // scroll-factor 1.0
-                  // scroll-method "no-scroll"
-                  // scroll-button 273
-                  // left-handed
-                  // middle-emulation
+                // off
+                // natural-scroll
+                // accel-speed 0.2
+                // accel-profile "flat"
+                // scroll-factor 1.0
+                // scroll-method "no-scroll"
+                // scroll-button 273
+                // left-handed
+                // middle-emulation
               }
 
               trackpoint {
-                  // off
-                  // natural-scroll
-                  // accel-speed 0.2
-                  // accel-profile "flat"
-                  // scroll-method "on-button-down"
-                  // scroll-button 273
-                  // middle-emulation
+                // off
+                // natural-scroll
+                // accel-speed 0.2
+                // accel-profile "flat"
+                // scroll-method "on-button-down"
+                // scroll-button 273
+                // middle-emulation
               }
 
               trackball {
-                  // off
-                  // natural-scroll
-                  // accel-speed 0.2
-                  // accel-profile "flat"
-                  // scroll-method "on-button-down"
-                  // scroll-button 273
-                  // left-handed
-                  // middle-emulation
+                // off
+                // natural-scroll
+                // accel-speed 0.2
+                // accel-profile "flat"
+                // scroll-method "on-button-down"
+                // scroll-button 273
+                // left-handed
+                // middle-emulation
               }
 
               tablet {
-                  // off
-                  map-to-output "eDP-1"
-                  // left-handed
+                // off
+                map-to-output "eDP-1"
+                // left-handed
               }
 
               touch {
-                  map-to-output "eDP-1"
+                map-to-output "eDP-1"
               }
 
               // disable-power-key-handling
@@ -337,7 +337,7 @@
               // workspace-auto-back-and-forth
             }
             output "eDP-1" {
-               scale 2.0
+              scale 2.0
             }
             binds {
 
@@ -359,6 +359,9 @@
               // Run `wpctl set-volume @DEFAULT_AUDIO_SINK@ 0.1+`.
               XF86AudioRaiseVolume { spawn "wpctl" "set-volume" "-l" "1.0" "@DEFAULT_AUDIO_SINK@" "5%+"; }
               XF86AudioLowerVolume { spawn "wpctl" "set-volume" "-l" "1.0" "@DEFAULT_AUDIO_SINK@" "5%-"; }
+              XF86MonBrightnessDown { spawn "brightnessctl" "set" "5%-"; }
+              XF86MonBrightnessUp { spawn "brightnessctl" "set" "5%+"; }
+
 
               XF86AudioMute allow-when-locked=true { spawn "wpctl" "set-mute" "@DEFAULT_AUDIO_SINK@" "toggle"; }
             }
