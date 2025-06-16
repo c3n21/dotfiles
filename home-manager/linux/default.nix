9f3cf9ded633ee509accdc102d4e0b3897b03084
c3n21 <37077738+c3n21@users.noreply.github.com>
2025-06-16 12:50:26 +0200

    librewolf

--- home-manager/linux/default.nix
+++ home-manager/linux/default.nix
@@ -240,5 +240,18 @@ rec {
       enableFishIntegration = true;
       tmux.enableShellIntegration = true;
     };
+
+    librewolf = {
+      enable = true;
+      # Enable WebGL, cookies and history
+      settings = {
+        "webgl.disabled" = false;
+        "privacy.resistFingerprinting" = false;
+        "privacy.clearOnShutdown.history" = false;
+        "privacy.clearOnShutdown.cookies" = false;
+        "network.cookie.lifetimePolicy" = 0;
+      };
+    };
+
   };
 }
