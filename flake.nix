ff1b3a2ccd76be4427ef7b22e4104218aa434311
c3n21 <37077738+c3n21@users.noreply.github.com>
2025-07-25 10:21:15 +0200

    fix!(flake): disable disko temporarily

--- flake.nix
+++ flake.nix
@@ -93,7 +93,8 @@
 
             ./nixos/framework-13-7040-amd/hardware-configuration.nix
             ./nixos/framework-13-7040-amd/configuration.nix
-            ./nixos/framework-13-7040-amd/disko.nix
+            # TODO: enable when the config is ready
+            # ./nixos/framework-13-7040-amd/disko.nix
 
             ./nixos/firewall.nix
             ./nixos/desktop.nix
