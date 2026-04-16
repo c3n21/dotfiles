d94b24b61e300126e0985c63132e59076fe2cee5
c3n21 <37077738+c3n21@users.noreply.github.com>
2026-04-16 21:53:49 +0200

    refactor(flake): remove dead code

--- flake.nix
+++ flake.nix
@@ -139,68 +139,6 @@
           ];
         };
 
-        workstation = nixpkgs.lib.nixosSystem {
-          inherit system;
-
-          specialArgs = {
-            inherit inputs outputs;
-          };
-
-          modules = [
-            disko.nixosModules.disko
-            ./nixos/common/fish.nix
-            ./nixos/common/nixpkgs-configuration.nix
-            ./nixos/common/distributed-builds.nix
-
-            ./nixos/workstation
-            # TODO: enable when the config is ready
-            # ./nixos/framework-13-7040-amd/disko.nix
-
-            ./nixos/firewall.nix
-            ./nixos/desktop.nix
-            ./nixos/specialisations.nix
-            home-manager.nixosModules.home-manager
-            homeManagerModuleConfiguration
-            {
-              home-manager.users.zhifan = ./home-manager/home.nix;
-            }
-
-            {
-              home-manager.users.zhifan = ./home-manager/linux/packages-profiles/gaming.nix;
-            }
-
-            {
-
-              # home-manager modules must be put there
-              home-manager.users.zhifan.imports = [
-                inputs.niri.homeModules.niri
-              ];
-            }
-
-            {
-              home-manager.users.zhifan = ./home-manager/linux;
-            }
-
-            {
-              specialisation.niri.configuration.home-manager.users.zhifan =
-                ./home-manager/linux/specialisations/niri.nix;
-              specialisation.hyprland.configuration.home-manager.users.zhifan =
-                ./home-manager/linux/specialisations/hyprland.nix;
-            }
-
-            {
-              environment.sessionVariables.NIXOS_OZONE_WL = "1";
-            }
-
-            {
-              nixpkgs.overlays = [
-                niri.overlays.niri
-              ];
-
-            }
-          ];
-        };
-
         wsl = nixpkgs.lib.nixosSystem {
           system = "x86_64-linux";
           modules = [
