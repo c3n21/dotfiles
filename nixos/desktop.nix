2ae5526fdfd64eef9d39c975475ef11d9bfaf422
c3n21 <37077738+c3n21@users.noreply.github.com>
2026-07-13 12:19:49 +0200

    feat(kanata): add scrolling layer

--- nixos/desktop.nix
+++ nixos/desktop.nix
@@ -42,9 +42,36 @@ rec {
           config =
             # lisp
             ''
-              (defsrc caps esc)
-              (deflayer base esc caps)
+              (defsrc
+                caps esc c h j k l
+              )
+
+              (defalias
+                scroll-layer
+                  (layer-while-held scrolling)
+
+                scroll-left
+                  (mwheel-accel-left 3 1200 1.15 0.93)
+
+                scroll-down
+                  (mwheel-accel-down 3 1200 1.15 0.93)
+
+                scroll-up
+                  (mwheel-accel-up 3 1200 1.15 0.93)
+
+                scroll-right
+                  (mwheel-accel-right 3 1200 1.15 0.93)
+              )
+
+              (deflayer base
+                esc @scroll-layer c h j k l
+              )
+
+              (deflayer scrolling
+                _ _ caps @scroll-left @scroll-down @scroll-up @scroll-right
+              )
             '';
+
           devices = [
             "/dev/input/by-path/platform-i8042-serio-0-event-kbd" # framework-13-7040-amd
           ];
