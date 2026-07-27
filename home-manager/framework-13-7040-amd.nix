94246dce8aeabd50701c19cc6b091cfbc5a808db
c3n21 <37077738+c3n21@users.noreply.github.com>
2026-07-27 08:08:19 +0000

    feat(framework-13-7040-amd/opencode): add custom llama-server

--- home-manager/framework-13-7040-amd.nix
+++ home-manager/framework-13-7040-amd.nix
@@ -41,4 +41,25 @@
       indicator = true;
     };
   };
+
+  programs.opencode.settings = {
+    "provider" = {
+      "llama.cpp" = {
+        "npm" = "@ai-sdk/openai-compatible";
+        "name" = "llama-server (local)";
+        "options" = {
+          "baseURL" = "http://192.168.1.192:8080/v1";
+        };
+        "models" = {
+          "bartowski/Qwen_Qwen3-30B-A3B-Instruct-2507-GGUF:Q4_K_M" = {
+            "name" = "bartowski/Qwen_Qwen3-30B-A3B-Instruct-2507-GGUF:Q4_K_M";
+            "limit" = {
+              "context" = 16384;
+              "output" = 8192;
+            };
+          };
+        };
+      };
+    };
+  };
 }
