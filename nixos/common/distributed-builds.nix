f5184d0a73efee83be6433618662dc6ee0a4d272
c3n21 <37077738+c3n21@users.noreply.github.com>
2026-07-27 12:30:54 +0200

    fix!(distributed-builds): remove coordinator as remote builder

--- nixos/common/distributed-builds.nix
+++ nixos/common/distributed-builds.nix
@@ -8,31 +8,31 @@
   };
 
   nix.buildMachines = [
-    {
-      # Remote host must be added to /root/.ssh/known_hosts
-      # otherwise it will throw a generic error about not being able to start SSH connection
-      hostName = "coordinator.private.headscale.com";
-      sshUser = "remotebuilder";
-      sshKey = "/root/.ssh/remotebuilder";
-
-      # Supported build architectures
-      systems = [
-        "x86_64-linux"
-        "aarch64-linux"
-        "i686-linux"
-      ];
-      supportedFeatures = [
-        "nixos-test"
-        "big-parallel"
-        "kvm"
-        # "benchmark"
-      ];
-      protocol = "ssh-ng";
-      maxJobs = 2;
-      speedFactor = 8;
-      mandatoryFeatures = [ ];
-
-    }
+    # {
+    #   # Remote host must be added to /root/.ssh/known_hosts
+    #   # otherwise it will throw a generic error about not being able to start SSH connection
+    #   hostName = "coordinator.private.headscale.com";
+    #   sshUser = "remotebuilder";
+    #   sshKey = "/root/.ssh/remotebuilder";
+    #
+    #   # Supported build architectures
+    #   systems = [
+    #     "x86_64-linux"
+    #     "aarch64-linux"
+    #     "i686-linux"
+    #   ];
+    #   supportedFeatures = [
+    #     "nixos-test"
+    #     "big-parallel"
+    #     "kvm"
+    #     # "benchmark"
+    #   ];
+    #   protocol = "ssh-ng";
+    #   maxJobs = 2;
+    #   speedFactor = 8;
+    #   mandatoryFeatures = [ ];
+    #
+    # }
     {
       # Remote host must be added to /root/.ssh/known_hosts
       # otherwise it will throw a generic error about not being able to start SSH connection
